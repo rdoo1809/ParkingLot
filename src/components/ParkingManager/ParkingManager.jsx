7d04b5e502599ff552c9531a75b75ca58fc9f902
Ryan Dooley <r1d8o0o9@gmail.com>
2024-07-09 15:33:36 -0400

    prior to trying timer stuff

--- src/components/ParkingManager/ParkingManager.jsx
+++ src/components/ParkingManager/ParkingManager.jsx
@@ -1,23 +1,58 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 
 const ParkingManager = () => {
+    const [parking, setParking] = useState(3);
+    const [currentPlate, setCurrentPlate] = useState('');
+    const [activeCars, setActiveCars] = useState([]);
+
+
+    const Timer = ({initialSeconds}) => {
+        const [seconds, setSeconds] = useState(initialSeconds);
+        useEffect(() => {
+            const interval = setInterval(() => {
+
+                //setParking(prevParking => prevParking + 1);
+                //perform calc
+
+
+            }, 1000);
+
+            return () => clearInterval(interval); // Cleanup interval on component unmount
+        }, []);
+    };
+
+    const handleInputChange = (event) => {
+        setCurrentPlate(event.target.value);
+    };
+
+    const enterPlate = () => {
+        //decrement parking
+        setParking(parking - 1);
+
+        //add current plate to array of cars
+        let newCar = [currentPlate, new Timer(10000)];
+        setActiveCars([...activeCars, newCar]);
+
+        //update table
+        console.log(activeCars);
+    }
 
 
     return (
         <div className={'bg-teal-500 w-2/3 p-4 mt-6'}>
-            <p className="text-3xl font-bold">ParkEz-5000</p>
-
+            <p className="text-3xl font-bold">ParkEz-5001</p>
             <div>
-                <p className={'text-left font-bold'}>Available Spots - 3/3</p>
+                <p className={'text-left font-bold'}>Available Spots - {parking}/3</p>
             </div>
             <div>
                 <label className={'text-left font-bold'}>Enter License Plate:</label>
-                <input className={'mx-2'} type={'text'}/>
+                <input onChange={handleInputChange}
+                       className={'mx-2'} type={'text'}/>
                 <input type={"submit"} value={"Enter"}
-                       className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"/>
+                       className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+                       onClick={enterPlate}/>
             </div>
-
             <div className={'mt-4'}>
                 <table className="min-w-full divide-y divide-gray-200">
                     <thead>
@@ -30,14 +65,27 @@ const ParkingManager = () => {
                         </th>
                     </tr>
                     </thead>
-                    <tbody className="bg-white divide-y divide-gray-200">
+                    <tbody className="bg-white divide-y divide-gray-500">
                     <tr>
                         <td className="px-4 py-4 uppercase">axbh700</td>
                         <td className="px-6 py-4">$1.50</td>
                         <td className="px-6 py-4">
-                            <a className={'text-red-600 font-bold'}>X</a>
+                            <a href={'/'} className={'text-red-600 font-bold'}>X</a>
                         </td>
                     </tr>
+                    {
+                        activeCars.map((car, index) => {
+                            return (
+                                <tr key={index}>
+                                    <td className="px-4 py-4 uppercase">{car[0]}</td>
+                                    <td className="px-6 py-4">{car[1]}</td>
+                                    <td className="px-6 py-4">
+                                        <a href={'/'} className={'text-red-600 font-bold'}>X</a>
+                                    </td>
+                                </tr>
+                            );
+                        })
+                    }
                     </tbody>
                 </table>
             </div>
