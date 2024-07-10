8bcbd15d215d3614b280dfd64357b217cb2e0aff
Ryan Dooley <r1d8o0o9@gmail.com>
2024-07-09 20:56:31 -0400

    complete

--- src/components/ParkingManager/ParkingManager.jsx
+++ src/components/ParkingManager/ParkingManager.jsx
@@ -6,37 +6,57 @@ const ParkingManager = () => {
     const [parking, setParking] = useState(3);
     const [currentPlate, setCurrentPlate] = useState('');
     const [activeCars, setActiveCars] = useState([]);
+    const [exitCost, setExitCost] = useState('');
 
     const handleInputChange = (event) => {
         setCurrentPlate(event.target.value);
     };
 
     const enterPlate = () => {
-        //decrement parking
-        setParking(parking - 1);
+        if (parking === 0) {
+            alert("Lot Full. No spots available.");
+        } else {
+            //decrement parking
+            setParking(parking - 1);
 
-        //add current plate to array of cars
-        let newCar = [currentPlate, 'timer'];
-        setActiveCars([...activeCars, newCar]);
+            //add current plate to array of cars
+            let newCar = [currentPlate, 'timer'];
+            setActiveCars([...activeCars, newCar]);
 
-        //update table
-        console.log(activeCars);
+            //update table - clear form
+            console.log(activeCars);
+        }
+        setCurrentPlate('');
     }
 
+    const exitCar = (cost, index) => {
+        //get exitCost for the specific car
+        setExitCost(cost);
+
+        //alert attendant of car leaving and cost to charge
+        alert(`Car exiting lot. \nPlate - ${activeCars[index][0]} \nCost: ${cost}`);
+
+        //increment available spots
+        setParking(parking + 1);
+
+        //remove from array of cars
+        setActiveCars(activeCars.filter((car, i) => i !== index));
+    }; //cost is undefined on first run
 
     return (
-        <div className={'bg-teal-500 w-2/3 p-4 mt-6'}>
+        <div className={'bg-teal-500 w-2/3 p-10 mt-6 rounded-2xl shadow-2xl'}>
             <p className="text-3xl font-bold">ParkEz-5001</p>
             <div>
                 <p className={'text-left font-bold'}>Available Spots - {parking}/3</p>
             </div>
             <div>
                 <label className={'text-left font-bold'}>Enter License Plate:</label>
-                <input onChange={handleInputChange}
+                <input onChange={handleInputChange} value={currentPlate}
                        className={'mx-2'} type={'text'}/>
                 <input type={"submit"} value={"Enter"}
                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-                       onClick={enterPlate}/>
+                       onClick={enterPlate}
+                       disabled={currentPlate === ''}/>
             </div>
             <div className={'mt-4'}>
                 <table className="min-w-full divide-y divide-gray-200">
@@ -51,22 +71,15 @@ const ParkingManager = () => {
                     </tr>
                     </thead>
                     <tbody className="bg-white divide-y divide-gray-500">
-                    <tr>
-                        <td className="px-4 py-4 uppercase">axbh700</td>
-                        <td className="px-6 py-4">$1.50</td>
-                        <td className="px-6 py-4">
-                            <a href={'/'} className={'text-red-600 font-bold'}>X</a>
-                        </td>
-                    </tr>
                     {
                         activeCars.map((car, index) => {
                             return (
                                 <tr key={index}>
                                     <td className="px-4 py-4 uppercase">{car[0]}</td>
-                                    <TimeKeeper />
-                                    <td className="px-6 py-4">
-                                        <a href={'/'} className={'text-red-600 font-bold'}>X</a>
-                                    </td>
+                                    <TimeKeeper index={index} onExitCar={exitCar}/>
+                                    {/*<td className="px-6 py-4">*/}
+                                    {/*    <button onClick={()=>exitCar(index)} className={'text-red-600 font-bold'}>X</button>*/}
+                                    {/*</td>*/}
                                 </tr>
                             );
                         })
--- src/components/TimeKeeper/TimeKeeper.jsx
+++ src/components/TimeKeeper/TimeKeeper.jsx
@@ -1,13 +1,13 @@
 import React, {useEffect, useState} from 'react';
 
-const TimeKeeper = () => {
+const TimeKeeper = ({index, onExitCar}) => {
     const [seconds, setSeconds] = useState(0);
     const [cost, setCost] = useState('$0.00')
 
     useEffect(() => {
         const interval = setInterval(() => {
             setSeconds(prevSeconds => prevSeconds + 1);
-            if (seconds >= 30) {
+            if (seconds >= 120) {
                 setCost('DAILY MAX - $4.00');
                 clearInterval(interval);
                 return;
@@ -16,15 +16,21 @@ const TimeKeeper = () => {
             // per second approx = 0.033
             let goingRate = `\$${(0.033 * seconds).toFixed(2)}`;
             setCost(goingRate);
-
         }, 1000);
         return () => clearInterval(interval);
     }, [seconds]);
 
+    const exitCar = (index) => {
+        // Notify parent component of the cost when exiting
+        onExitCar(cost, index);
+    };
 
     return (
         <>
-            <td className="px-6 py-4">{cost}</td>
+            <td  className="px-6 py-4">{cost}</td>
+            <td>
+                <button onClick={() => exitCar(index)} className={'text-red-600 font-bold'}>X</button>
+            </td>
         </>
     );
 }
